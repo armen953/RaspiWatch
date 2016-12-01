7f4789f230f13a7dc02ec602630c93687594716d
Pinto Raphaël <raphael.pinto@iut-dijon.u-bourgogne.fr>
2016-12-01 19:08:54 +0100

    Beautify

--- dashboard.php
+++ dashboard.php
@@ -12,177 +12,182 @@ if (isset($_GET['id']) AND $_GET['id'] > 0)  // verfier si la variable id existe
 ?>
 
 
-<!DOCTYPE html>
-<html lang="fr">
+    <!DOCTYPE html>
+    <html lang="fr">
 
-<head>
+    <head>
 
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" href="images/favicon.ico" />
-    <meta name="description" content="">
-    <meta name="author" content="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" href="images/favicon.ico" />
+        <meta name="description" content="">
+        <meta name="author" content="">
 
-    <title>Dashboard</title>
+        <title>Dashboard</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Custom CSS -->
-    <link href="assets/css/sb-admin.css" rel="stylesheet">
-    <!-- Morris Charts CSS -->
-    <link href="assets/css/plugins/morris.css" rel="stylesheet">
-    <!-- Custom Fonts -->
-    <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+        <!-- Bootstrap Core CSS -->
+        <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Custom CSS -->
+        <link href="assets/css/sb-admin.css" rel="stylesheet">
+        <!-- Morris Charts CSS -->
+        <link href="assets/css/plugins/morris.css" rel="stylesheet">
+        <!-- Custom Fonts -->
+        <link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-</head>
-<body>
+    </head>
+
+    <body>
 
 
-<?php  // affiche du contenu propre a l'utlisateur (si l'ulisateur connecté veux visiter le profil d'un autre cela va afficher ce qu'il y a dans le else
+        <?php  // affiche du contenu propre a l'utlisateur (si l'ulisateur connecté veux visiter le profil d'un autre cela va afficher ce qu'il y a dans le else
 		if (isset($_SESSION['id']) AND $userInfo['id'] == $_SESSION['id'])    // on verfie si l'id du user est le meme que celui de la session
 		{     // ouverture de if numero 2
 ?>
 
 
 
-	<div id="wrapper">
+            <div id="wrapper">
 
-		<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 
-			<div class="navbar-header">
+                    <div class="navbar-header">
 
-	                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-	                    <span class="sr-only">Toggle navigation</span>
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                    <span class="icon-bar"></span>
-	                </button>
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">RaspiWatch</a>
+                    </div>
 
-	                <a class="navbar-brand" href="#">Dashboard</a>
-	        </div>
-
-	        <ul class="nav navbar-right top-nav">
-	        	<!-- le menu profil  -->
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $userInfo['pseudo'] ?> <b class="caret"></b></a>
-                    <ul class="dropdown-menu">
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
-                        </li>
-                        <li class="divider"></li>
-                        <li>
-                            <a href="php/deconnexion.php"><i class="fa fa-fw fa-power-off"></i> Déconnexion</a>
+                    <ul class="nav navbar-right top-nav">
+                        <!-- le menu profil  -->
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> <?php echo $userInfo['pseudo'] ?> <b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li>
+                                    <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
+                                </li>
+                                <li>
+                                    <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
+                                </li>
+                                <li class="divider"></li>
+                                <li>
+                                    <a href="php/deconnexion.php"><i class="fa fa-fw fa-power-off"></i> Déconnexion</a>
+                                </li>
+                            </ul>
                         </li>
                     </ul>
-                </li>
-            </ul>
 
-            <!-- Menu a gauche  -->
-             <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav side-nav">
-                    <li class="active">
-                        <a href="#"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
-                    </li>
-                    <li>
-                        <?php echo "<a href="."controlMotion.php?id=".$userInfo['id']."><i class="."fa fa-fw fa-wrench"."></i> Config Motion</a>" ?>
-                    </li>
-                    <li>
-                        <a href="#"><i class="fa fa-fw fa-table"></i> Tables</a>
-                    </li>
-                </ul>
-            </div>
-		</nav>
+                    <!-- Menu a gauche  -->
+                    <div class="collapse navbar-collapse navbar-ex1-collapse">
+                        <ul class="nav navbar-nav side-nav">
+                            <li class="active">
+                                <?php echo "<a href="."dashboard.php?id=".$userInfo['id']?> "><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
+                            </li>
+                            <li>
+                                <?php echo "<a href="."controlMotion.php?id=".$userInfo['id']?> "><i class="fa fa-fw fa-wrench"></i> Config Motion</a>
+                            </li>
+                            <li>
+                                <?php echo "<a href="."images.php?id=".$userInfo['id']?> "><i class="fa fa-fw fa-picture-o"></i> Images</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
 
-        <div id="page-wrapper">
+                <div id="page-wrapper">
 
-            <div class="container-fluid">
+                    <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
+                        <!-- Page Heading -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1 class="page-header">
                             Dashboard <small>Aperçu des caméras</small>
                         </h1>
+                                <ol class="breadcrumb">
+                                    <li class="active">
+                                        <i class="fa fa-dashboard"></i> Dashboard
+                                    </li>
+                                </ol>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <div class="row">
+                                        <div class="embed-responsive embed-responsive-16by9">
+                                            <iframe class="embed-responsive-item" width="560" height="315" src="https://www.youtube.com/embed/6fI91Fh9Qgo" scrolling="no" allowfullscreen></iframe>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                            <!-- recherche -->
+                            <div class="col-lg-3 form-group input-group">
+                                <input type="text" class="form-control">
+                                <span class="input-group-btn"><button class="btn btn-default" type="button"><i class="fa fa-search"></i></button></span>
+                            </div>
+
+
+
+
+
+
+
+
+                        </div>
                     </div>
+
+
+
+
+
+
+
+
+
+
                 </div>
 
-                <div class="row">
-                        <div class="panel panel-primary">
-                           <div class="panel-heading">
-                                <div class="row">
-					<div class="embed-responsive embed-responsive-16by9">
-						<iframe class="embed-responsive-item" src="http://172.23.40.235:8081/?action=stream" scrolling="no" allowfullscreen></iframe>				
-                                	</div>
-                            	</div>
-                          </div>
-                    	</div>
+
+                <!-- jQuery -->
+                <script src="assets/js/jquery.js"></script>
+
+                <!-- Bootstrap Core JavaScript -->
+                <script src="assets/js/bootstrap.min.js"></script>
+
+                <!-- Morris Charts JavaScript -->
+                <script src="assets/js/plugins/morris/raphael.min.js"></script>
+                <script src="assets/js/plugins/morris/morris.min.js"></script>
+                <script src="assets/js/plugins/morris/morris-data.js"></script>
 
 
-
-				<!-- recherche -->
-				<div class="col-lg-3 form-group input-group">
-                    <input type="text" class="form-control">
-                    <span class="input-group-btn"><button class="btn btn-default" type="button"><i class="fa fa-search"></i></button></span>
-                 </div>
-
-
-
-
-
-
-
-
-            </div>
-        </div>
-
-
-
-
-
-
-
-
-
-
-
-	</div>
-
-
- <!-- jQuery -->
-    <script src="assets/js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="assets/js/bootstrap.min.js"></script>
-
-    <!-- Morris Charts JavaScript -->
-    <script src="assets/js/plugins/morris/raphael.min.js"></script>
-    <script src="assets/js/plugins/morris/morris.min.js"></script>
-    <script src="assets/js/plugins/morris/morris-data.js"></script>
-
-
-		<?php
+                <?php
 			}   // fermeture  de if numero 2
 	        else{
 	            echo '<img class="pasdispo" src="images/pagePasDispo.png" alt="" > ';
 	        }
 	        ?>
-	     </body>
-	 </html>
+    </body>
 
-	 <?php
+    </html>
+
+    <?php
  }    // fermeture  de if numero 1
 
 	 ?>
