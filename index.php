e1b6e2655bafb1346987b3263bec3f89da4d5c4c
Pinto Raphaël <raphael.pinto@iut-dijon.u-bourgogne.fr>
2016-12-01 19:09:37 +0100

    Beautify

--- index.php
+++ index.php
@@ -1,42 +1,45 @@
 <!DOCTYPE HTML>
 <html>
-	<head>
-		<title>RaspiWatch</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<link rel="stylesheet" href="assets/css/index.css" />
-		<link rel="icon" href="images/favicon.ico" />
-	</head>
-	<body class="homepage">
-		<div id="page-wrapper">
 
-			<!-- Header -->
-				<div id="header">
+<head>
+    <title>RaspiWatch</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/index.css" />
+    <link rel="icon" href="images/favicon.ico" />
+</head>
 
-					<!-- Inner -->
-						<div class="inner">
-							<header>
-								<h1><a href="index.php" id="logo">RaspiWatch</a></h1>
-								<hr />
-								<p>Systeme de surveillance à distance</p>
-							</header>
-							<footer>
-								<a href="connexion.php" class="button circled scrolly">Connexion</a>
-							</footer>
-						</div>
+<body class="homepage">
+    <div id="page-wrapper">
 
-				</div>
-		</div>
-		<!-- Scripts -->
-		<div>
-			<script src="assets/js/jquery.min.js"></script>
-			<script src="assets/js/jquery.dropotron.min.js"></script>
-			<script src="assets/js/jquery.scrolly.min.js"></script>
-			<script src="assets/js/jquery.onvisible.min.js"></script>
-			<script src="assets/js/skel.min.js"></script>
-			<script src="assets/js/util.js"></script>
-			<script src="assets/js/main.js"></script>
-		</div>
+        <!-- Header -->
+        <div id="header">
 
-	</body>
+            <!-- Inner -->
+            <div class="inner">
+                <header>
+                    <h1><a href="index.php" id="logo">RaspiWatch</a></h1>
+                    <hr />
+                    <p>Systeme de surveillance à distance</p>
+                </header>
+                <footer>
+                    <a href="connexion.php" class="button circled scrolly">Connexion</a>
+                </footer>
+            </div>
+
+        </div>
+    </div>
+    <!-- Scripts -->
+    <div>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/js/jquery.dropotron.min.js"></script>
+        <script src="assets/js/jquery.scrolly.min.js"></script>
+        <script src="assets/js/jquery.onvisible.min.js"></script>
+        <script src="assets/js/skel.min.js"></script>
+        <script src="assets/js/util.js"></script>
+        <script src="assets/js/main.js"></script>
+    </div>
+
+</body>
+
 </html>
