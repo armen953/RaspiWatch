f9f6dcf4cbb26d4e3f4f830ff2dc380de6fb3ea5
Armen MAHMEDOV <armen95@hotmail.fr>
2017-01-14 20:48:54 +0100

    Ajout suppression de l'image dans le dossier
    Lorsqu'on supprime une image cela supprime le lien dans la bdd et aussi l'image dans le dossier

--- php/supprimerImage.php
+++ php/supprimerImage.php
@@ -6,9 +6,11 @@ $bdd = new connexion();
 if (isset($_SESSION['id']) AND  isset($_GET['nom']) AND $_SESSION['id']=6) 
 {
 	$imageASupprimer = $_GET['nom'];
+	$nomImg = substr($_GET['nom'],29); // 29 a modifier pour mettre la valeur du lien de la pi
 	$reqImage = $bdd->getConnexion()->prepare('DELETE FROM security WHERE filename="'.$imageASupprimer.'"');
 	$reqImage->execute();
 	$_SESSION['imageDel'] = true;
+	unlink("../".$nomImg."");  // supprime l'image du ficher (on pass le lien ../imgCamera1/nom.jpg)
 	header("Location: ../images.php?id=".$_SESSION['id']."");
 	
 }else
